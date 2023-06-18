b4fd03c135a86da19a2eaf6503851df36a55fe4f
Tudor Paul <ionelpaul2001@gmail.com>
2023-06-18 17:53:16 +0300

    finished first react static app

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-function Navbar(){
-    return(
-        <div id="main">
-<nav className="navbar navbar-default">
-  <ul className="nav navbar-nav">
-    <li><a href="#0">Home</a></li>
-    <li><a href="#0">About</a></li>
-    <li className="dropdown">
-      <a href="#0" className="dropdown-toggle" data-toggle="dropdown" data-target="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Clients<span class="caret"></span></a>
-      <ul className="dropdown-menu" id="dropdon-menu">
-        <li><a href="#0">Burger King</a></li>
-        <li><a href="#0">Southwest Airlines</a></li>
-        <li><a href="#0">Levi Strauss</a></li>
-      </ul>
-    </li>
-    <li class="dropdown">
-      <a href="#0" className="dropdown-toggle" data-toggle="dropdown" data-target="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services<span class="caret"></span></a>
-      <ul class="dropdown-menu" id="dropdon-menu">
-        <li><a href="#0">Print Design</a></li>
-        <li><a href="#0">Web Design</a></li>
-        <li><a href="#0">Mobile App Development</a></li>
-      </ul>
-    </li>
-    <li><a href="#0">Contact</a></li>
-  </ul>
-</nav>
-</div>
-    )
+function Header(){
+  return <header><nav className="nav"><img className="img" src="/react.png"></img><div></div><ul className="nav-items"><li>Price</li><li>Contact</li><li>About</li></ul></nav></header>;
 }
-function Component(){
-    return(<main><h1>Sal cf</h1><p>bn fac</p></main>)
+function Maindata(){
+  return<div className="main-container"><h1>This is my first static web page</h1><ul><li>this is an element</li><li>this is another element</li></ul></div>
 }
-ReactDOM.render(<div><Navbar /> <Component /></div>,document.getElementById('root'));
+function Footer(){
+  return <footer>@2023 Ionel Tudor-Paul.All rights reserved</footer>
+}
+function All(){
+  return <><Header /><Maindata /><Footer /></>
+}
+ReactDOM.render(<All />,document.getElementById('root'));
